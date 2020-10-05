449372fc934a140ccf589ac993e409a01db9a842
ron <Nelly Chelaa>
2020-10-06 02:53:14 +0300

    Completed module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
-import {ProductService} from './product.service'
+import {ProductService} from './product.service';
+import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductPageComponent
+    ProductPageComponent,
+    ProductTracklistingComponent
   ],
   imports: [
     BrowserModule,
